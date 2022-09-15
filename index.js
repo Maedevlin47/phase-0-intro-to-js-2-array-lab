c11a07b2a2ccf41ad88f3be0e507bd74b4d2a031
Mae Schreck <maeschreck@Maes-MacBook-Pro.local>
2022-09-14 22:17:27 -0400

    commit tree fab28e1113ba47146ff98ac7b3a772e24f07a8dc

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+
+
+const copyOfCatsUsingSpreadOperator = [...cats];
+
+const copyOfCatsUsingSlice = cats.slice();
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+function appendCat(name){
+    let appendCat = "Broom";
+    return [...cats, "Broom"];
+    
+    }
+
+function prependCat(name){
+    let prependCat = "Arnold";
+    return ["Arnold", ...cats];
+    }
+
+function removeLastCat(){
+    let removeLastCat = cats.slice();
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    let removeFirstCat = cats.slice();
+    return cats.slice(1);
+}
